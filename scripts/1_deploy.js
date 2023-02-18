40c974c0b701779ec7f37deefa874cf5295b4458
Red-0510 <jaineshmachhi@gmail.com>
2023-02-19 02:28:23 +0530

    compiled sample token

---
+++ scripts/1_deploy.js
@@ -0,0 +1,18 @@
+// const { ethers } = require("hardhat");
+
+async function main(){
+    //Fetch the contract
+    const Token = await ethers.getContractFactory("Token");
+
+    //deploy the contract
+    const token = await Token.deploy();
+    await token.deployed();
+    console.log(`Token deployed to: ${token.address}`);
+}
+
+main()
+  .then(()=>process.exit(0))
+  .catch(error=>{
+    console.log(error);
+    process.exit(1);
+  });
